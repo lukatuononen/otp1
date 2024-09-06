fae5953c75b54f32b26389236f6cd03fb0071518
luka <luka.tuononen@metropolia.fi>
2024-09-06 10:47:40 +0300

    test done

--- src/TemperatureConverterTest.java
+++ src/TemperatureConverterTest.java
@@ -4,18 +4,18 @@ public class TemperatureConverterTest {
 
     @org.junit.Test
     public void fahrenheitToCelsius() {
-        assertEquals(-17.77777777777778, TemperatureConverter.fahrenheitToCelsius(0), 0.0001);
+        assertEquals(-17.77777778, TemperatureConverter.fahrenheitToCelsius(0), 0.01);
     }
 
     @org.junit.Test
     public void celsiusToFahrenheit() {
-        assertEquals(32, TemperatureConverter.celsiusToFahrenheit(0), 0.0001);
+        assertEquals(32, TemperatureConverter.celsiusToFahrenheit(0), 0.01);
     }
 
     @org.junit.Test
     public void isExtremeTemperature() {
-        assertTrue(TemperatureConverter.isExtremeTemperature(-50));
-        assertTrue(TemperatureConverter.isExtremeTemperature(60));
+        assertTrue(TemperatureConverter.isExtremeTemperature(-41));
+        assertTrue(TemperatureConverter.isExtremeTemperature(51));
         assertFalse(TemperatureConverter.isExtremeTemperature(0));
     }
 }
